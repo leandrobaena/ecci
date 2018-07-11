607260253259616441b91d39b1d4f048142c1edb
Leandro Baena Torres <leandrobaena@gmail.com>
2018-07-11 16:27:26 -0500

    Actualización BL

---
+++ src/bl/src/ecci/bl/SolicitudBL.java
@@ -0,0 +1,247 @@
+package ecci.bl;
+
+import ecci.dal.SolicitudDAL;
+import ecci.entidades.Escolaridad;
+import ecci.entidades.Profesion;
+import ecci.entidades.Solicitud;
+import ecci.entidades.SolicitudDetalle;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author
+ */
+public class SolicitudBL {
+
+    //<editor-fold defaultstate="collapsed" desc="Atributos">
+    /**
+     * Solicitud manipulada
+     */
+    private Solicitud solicitud;
+
+    /**
+     * Administrador de la persistencia
+     */
+    private SolicitudDAL solicitudDAL;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructores">
+    /**
+     * Crea una nueva solicitud con su administrador de persistencia
+     *
+     * @param id Identificador de la solicitud
+     * @param propiedadesBD Propiedades de conexión a la base de datos
+     * @throws IOException
+     */
+    public SolicitudBL(int id, Properties propiedadesBD) throws IOException {
+        this.solicitud = new Solicitud(id);
+        this.solicitudDAL = new SolicitudDAL(propiedadesBD);
+        this.solicitudDAL.setSolicitud(solicitud);
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Métodos">
+    /**
+     * Trae el identificador de la solicitud
+     *
+     * @return Identificador de la solicitud
+     */
+    public int getId() {
+        return this.solicitud.getId();
+    }
+
+    /**
+     * Cambia el identificador de la solicitud
+     *
+     * @param id Identificador de la solicitud
+     */
+    public void setId(int id) {
+        this.solicitud.setId(id);
+    }
+
+    /**
+     * Trae la fecha de la solicitud
+     *
+     * @return Fecha de la solicitud
+     */
+    public Date getFecha() {
+        return this.solicitud.getFecha();
+    }
+
+    /**
+     * Cambia la fecha de la solicitud
+     *
+     * @param fecha Nueva fecha de la solicitud
+     */
+    public void setFecha(Date fecha) {
+        this.solicitud.setFecha(fecha);
+    }
+
+    /**
+     * Trae el nombre del usuario que realiza la solicitud
+     *
+     * @return Nombre del usuario que realiza la solicitud
+     */
+    public String getNombre() {
+        return this.solicitud.getNombre();
+    }
+
+    /**
+     * Cambia el nombre del usuario que realiza la solicitud
+     *
+     * @param nombre Nuevo nombre del usuario que realiza la solicitud
+     */
+    public void setNombre(String nombre) {
+        this.solicitud.setNombre(nombre);
+    }
+
+    /**
+     * Trae el email del usuario que hace la solicitud
+     *
+     * @return Email del usuario que hace la solicitud
+     */
+    public String getEmail() {
+        return this.solicitud.getEmail();
+    }
+
+    /**
+     * Cambia el email del usuario que hace la solicitud
+     *
+     * @param email Nuevo email del usuario que hace la solicitud
+     */
+    public void setEmail(String email) {
+        this.solicitud.setEmail(email);
+    }
+
+    /**
+     * Trae el nivel de escolaridad del usuario que hace la solicitud
+     *
+     * @return Nivel de escolaridad del usuario que hace la solicitud
+     */
+    public Escolaridad getEscolaridad() {
+        return this.solicitud.getEscolaridad();
+    }
+
+    /**
+     * Cambia el nivel de escolaridad del usuario que hace la solicitud
+     *
+     * @param escolaridad Nuevo nivel de escolaridad del usuario que hace la
+     * solicitud
+     */
+    public void setEscolaridad(Escolaridad escolaridad) {
+        this.solicitud.setEscolaridad(escolaridad);
+    }
+
+    /**
+     * Trae la ocupación o profesión del usuario que hace la solicitud
+     *
+     * @return Ocupación o profesión del usuario que hace la solicitud
+     */
+    public Profesion getProfesion() {
+        return this.solicitud.getProfesion();
+    }
+
+    /**
+     * Cambia la ocupación o profesión del usuario que hace la solicitud
+     *
+     * @param profesion Ocupación o profesión del usuario que hace la solicitud
+     */
+    public void setProfesion(Profesion profesion) {
+        this.solicitud.setProfesion(profesion);
+    }
+
+    /**
+     * Trae el listado de solicitudes desde la base de datos
+     *
+     * @return Listado de solicitudes desde la base de datos
+     * @throws SQLException
+     * @throws java.text.ParseException
+     */
+    public ArrayList<Solicitud> listar() throws SQLException, ParseException {
+        return this.solicitudDAL.listar();
+    }
+
+    /**
+     * Inserta una nueva solicitud en la base de datos
+     *
+     * @throws SQLException
+     */
+    public void insertar() throws SQLException {
+        this.solicitudDAL.insertar();
+    }
+
+    /**
+     * Carga una solicitud desde la base de datos
+     *
+     * @throws SQLException
+     * @throws java.text.ParseException
+     */
+    public void cargar() throws SQLException, ParseException {
+        this.solicitudDAL.cargar();
+    }
+
+    /**
+     * Actualiza una solicitud en la base de datos
+     *
+     * @throws SQLException
+     */
+    public void actualizar() throws SQLException {
+        this.solicitudDAL.actualizar();
+    }
+
+    /**
+     * Elimina una solicitud en la base de datos
+     *
+     * @throws SQLException
+     */
+    public void eliminar() throws SQLException {
+        this.solicitudDAL.eliminar();
+    }
+
+    /**
+     * Trae el listado de detalles de esta solicitud
+     *
+     * @return Listado de detalles de esta solicitud
+     * @throws SQLException
+     */
+    public List<SolicitudDetalle> getDetalles() throws SQLException {
+        return this.solicitudDAL.getDetalles();
+    }
+
+    /**
+     * Inserta un nuevo detalle de una solicitud en la base de datos
+     *
+     * @param detalle Nuevo detalle a insertar
+     * @throws SQLException
+     */
+    public void insertarDetalle(SolicitudDetalle detalle) throws SQLException {
+        this.solicitudDAL.insertarDetalle(detalle);
+    }
+
+    /**
+     * Actualiza un detalle de una solicitud en la base de datos
+     *
+     * @param detalle Detalle de una solicitud que se actualizará
+     * @throws SQLException
+     */
+    public void actualizarDetalle(SolicitudDetalle detalle) throws SQLException {
+        this.solicitudDAL.actualizarDetalle(detalle);
+    }
+
+    /**
+     * Elimina un detalle de una solicitud en la base de datos
+     *
+     * @param detalle Detalle de una solicitud que se eliminará
+     * @throws SQLException
+     */
+    public void eliminarDetalle(SolicitudDetalle detalle) throws SQLException {
+        this.solicitudDAL.eliminarDetalle(detalle);
+    }
+    //</editor-fold>
+}
---
+++ src/bl/src/ecci/bl/UsuarioBL.java
@@ -0,0 +1,232 @@
+package ecci.bl;
+
+import ecci.dal.UsuarioDAL;
+import ecci.entidades.Grupo;
+import ecci.entidades.Usuario;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ *
+ * @author
+ */
+public class UsuarioBL {
+
+    //<editor-fold defaultstate="collapsed" desc="Atributos">
+    /**
+     * Usuario manipulado
+     */
+    private Usuario usuario;
+
+    /**
+     * Administrador de la persistencia
+     */
+    private UsuarioDAL usuarioDAL;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructores">
+    /**
+     * Crea un nuevo usuario con su administrador de persistencia
+     *
+     * @param id Identificador del usuario
+     * @param propiedadesBD Propiedades de conexión a la base de datos
+     * @throws IOException
+     */
+    public UsuarioBL(int id, Properties propiedadesBD) throws IOException {
+        this.usuario = new Usuario(id);
+        this.usuarioDAL = new UsuarioDAL(propiedadesBD);
+        this.usuarioDAL.setUsuario(usuario);
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Métodos">
+    /**
+     * Trae el identificador del usuario
+     *
+     * @return Identificador del usuario
+     */
+    public int getId() {
+        return this.usuario.getId();
+    }
+
+    /**
+     * Cambia el identificador del usuario
+     *
+     * @param id Nuevo identificador del usuario
+     */
+    public void setId(int id) {
+        this.usuario.setId(id);
+    }
+
+    /**
+     * Trae el login del usuario
+     *
+     * @return Login del usuario
+     */
+    public String getLogin() {
+        return this.usuario.getLogin();
+    }
+
+    /**
+     * Cambia el login del usuario
+     *
+     * @param login Nuevo login del usuario
+     */
+    public void setLogin(String login) {
+        this.usuario.setLogin(login);
+    }
+
+    /**
+     * Trae los nombres del usuario
+     *
+     * @return Nombres del usuario
+     */
+    public String getNombres() {
+        return this.usuario.getNombres();
+    }
+
+    /**
+     * Cambia los nombres del usuario
+     *
+     * @param nombres Nuevos nombres del usuario
+     */
+    public void setNombres(String nombres) {
+        this.usuario.setNombres(nombres);
+    }
+
+    /**
+     * Trae los apellidos del usuario
+     *
+     * @return Apellidos del usuario
+     */
+    public String getApellidos() {
+        return this.usuario.getApellidos();
+    }
+
+    /**
+     * Cambia los apellidos del usuario
+     *
+     * @param apellidos Nuevos apellidos del usuario
+     */
+    public void setApellidos(String apellidos) {
+        this.usuario.setApellidos(apellidos);
+    }
+
+    /**
+     * Trae si el usuario está o no activo
+     *
+     * @return Si el usuario está o no activo
+     */
+    public boolean isActivo() {
+        return this.usuario.isActivo();
+    }
+
+    /**
+     * Cambia si el usuario está o no activo
+     *
+     * @param activo Si el usuario está o no activo
+     */
+    public void setActivo(boolean activo) {
+        this.usuario.setActivo(activo);
+    }
+
+    /**
+     * Trae el listado de usuarios desde la base de datos
+     *
+     * @return Listado de usuarios desde la base de datos
+     * @throws SQLException
+     */
+    public ArrayList<Usuario> listar() throws SQLException {
+        return this.usuarioDAL.listar();
+    }
+
+    /**
+     * Inserta un nuevo usuario en la base de datos
+     *
+     * @param contrasenia Contraseña con la que se crea el usuario
+     * @throws SQLException
+     */
+    public void insertar(String contrasenia) throws SQLException {
+        this.usuarioDAL.insertar(contrasenia);
+    }
+
+    /**
+     * Carga un usuario desde la base de datos
+     *
+     * @throws SQLException
+     */
+    public void cargar() throws SQLException {
+        this.usuarioDAL.cargar();
+    }
+
+    /**
+     * Actualiza un usuario en la base de datos
+     *
+     * @throws SQLException
+     */
+    public void actualizar() throws SQLException {
+        this.usuarioDAL.actualizar();
+    }
+
+    /**
+     * Actualiza la contraseña de un usuario en la base de datos
+     *
+     * @param contrasenia Nueva contraseña del usuario
+     * @throws SQLException
+     */
+    public void actualizarContrasenia(String contrasenia) throws SQLException {
+        this.usuarioDAL.actualizarContrasenia(contrasenia);
+    }
+
+    /**
+     * Elimina un usuario de la base de datos
+     *
+     * @throws SQLException
+     */
+    public void eliminar() throws SQLException {
+        this.usuarioDAL.eliminar();
+    }
+
+    /**
+     * Inserta un usuario en un grupo en la base de datos
+     *
+     * @param grupo Grupo en el que se desea insertar al usuario
+     * @throws SQLException
+     */
+    public void insertarUsuarioEnGrupo(Grupo grupo) throws SQLException {
+        this.usuarioDAL.insertarUsuarioEnGrupo(grupo);
+    }
+
+    /**
+     * Elimina un usuario de un grupo en la base de datos
+     *
+     * @param grupo Grupo del que se desea eliminar al usuario
+     * @throws SQLException
+     */
+    public void eliminarUsuarioEnGrupo(Grupo grupo) throws SQLException {
+        this.usuarioDAL.eliminarUsuarioEnGrupo(grupo);
+    }
+
+    /**
+     * Trae los grupos en los que actualmente se encuentra el usuario
+     *
+     * @return Grupos en los que actualmente se encuentra el usuario
+     * @throws java.sql.SQLException
+     */
+    public ArrayList<Grupo> listarGruposActuales() throws SQLException {
+        return this.usuarioDAL.listarGruposActuales();
+    }
+
+    /**
+     * Trae los grupos en los que actualmente no se encuentra el usuario
+     *
+     * @return Grupos en los que actualmente no se encuentra el usuario
+     * @throws java.sql.SQLException
+     */
+    public ArrayList<Grupo> listarGruposExcluidos() throws SQLException {
+        return this.usuarioDAL.listarGruposExcluidos();
+    }
+    //</editor-fold>
+}
